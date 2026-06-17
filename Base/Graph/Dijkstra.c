ab46385fd1b870264f3380e4601d66210989dc65
Oscarxwy <Oscarxwy@users.noreply.github.com>
2026-06-17 23:10:09 +0800

    Dijkstra: add shortest path implementation (commented)

--- Base/Graph/Dijkstra.c
+++ Base/Graph/Dijkstra.c
@@ -13,3 +13,33 @@ typedef struct Node
     int edgesSize;
 }Node;
 
+/*
+HashSetNode* dijkstra(Node* head)
+{
+	HashSetNode* map = (HashSetNode*)malloc(sizeof(HashSetNode));
+	addToHashSetNode(map, head, 0);
+
+	HashSet* visited = (HashSet*)malloc(sizeof(HashSet));
+
+	Node* minnode = getMinDistanceAndUnSelectNode(map, visited);
+
+	while(minnode != NULL)
+	{
+		addToHashSet(visited, minnode);
+		for(int i = 0; i < minnode->edgesSize; i++)
+		{
+			Edge* edge = minnode->edge[i];
+			if(!containsInHashSetNode(map, edge->to))
+			{
+				addToHashSetNode(map, edge->to, edge->weight);
+			}	
+			addToHashSetNode(map, edge->to, min(map[edge->to]->distance, map[edge->from]->distance + edge->weight));
+		}
+		minnode = getMinDistanceAndUnSelectNode(map, visited);
+	}
+
+	free(visited);
+	free(minnode);
+	return map;
+}
+*/
